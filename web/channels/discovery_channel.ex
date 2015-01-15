f4fe676456091a258d741f3e7095dd19cab6d5b4
Guido Zuidhof <me@guido.io>
2015-01-15 18:05:53 +0100

    Request id in discovery get

--- web/channels/discovery_channel.ex
+++ web/channels/discovery_channel.ex
@@ -8,10 +8,10 @@ defmodule Excess.DiscoveryChannel do
 
   # Room lookup
 
-  def handle_in("get:room", %{"id" => room_id}, socket) do
+  def handle_in("get:room", %{"id" => room_id, "r" => request}, socket) do
     users = Excess.Api.get_users(room_id)
     Logger.info "LOOKUP user list of #{room_id}"
-    reply socket, "get:room", %{users: users}
+    reply socket, "get:room", %{users: users, r:request}
   end
 
   def handle_in("get:room", message, socket) do
